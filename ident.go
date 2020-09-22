32874e85ee89cd6c785f09e94a4825446bbf934e
Yasuhiro Manai <yasuhiro.manai@gmail.com>
2020-09-23 08:07:51 +0900

    Add undefined functions

--- ident.go
+++ ident.go
@@ -120,3 +120,37 @@ func (i *Ident) UnmarshalText(data []byte) error {
 func (i Ident) MarshalText() ([]byte, error) {
 	return []byte(i.Original), nil
 }
+
+var spaces = []rune{'_', ' ', ':', '-', '/'}
+
+func isSpace(c rune) bool {
+	for _, r := range spaces {
+		if r == c {
+			return true
+		}
+	}
+	return unicode.IsSpace(c)
+}
+
+func xappend(a []string, ss ...string) []string {
+	for _, s := range ss {
+		s = strings.TrimSpace(s)
+		for _, x := range spaces {
+			s = strings.Trim(s, string(x))
+		}
+		if _, ok := baseAcronyms[strings.ToUpper(s)]; ok {
+			s = strings.ToUpper(s)
+		}
+		if s != "" {
+			a = append(a, s)
+		}
+	}
+	return a
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
